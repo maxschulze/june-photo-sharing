47e83874dddb2cbc4375b114afa49f941a869504
Max Schulze <max@mo-stud.io>
2013-03-12 16:13:40 +0100

    changing the default generators

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module JunePhotoSharing
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -58,5 +58,10 @@ module JunePhotoSharing
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+      g.test_framework :rspec
+    end
   end
 end
