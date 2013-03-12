50c86ca67eb2462dc8491bba1926b570faa60ae9
Max Schulze <max@mo-stud.io>
2013-03-12 16:33:21 +0100

    adding the first sign in step

---
+++ features/step_definitions/admin_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am signed in as an admin$/) do
+  @admin = create(:admin, :password => VALID_PASSWORD, :password_confirmation => VALID_PASSWORD)
+  visit('/users/sign_in')
+  fill_in 'user_email', :with => @admin.email
+  fill_in 'user_password', :with => VALID_PASSWORD
+  click_button 'Sign in'
+end
+
+When(/^I go to the start page$/) do
+  visit('/')
+end
