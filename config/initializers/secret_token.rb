04f7c7d1c61245c3e3258fc98f73f8e804ede3a3
Max Schulze <max@mo-stud.io>
2013-03-12 15:53:06 +0100

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JunePhotoSharing::Application.config.secret_token = 'c219b9d7866521c5837521438b93d49c94b269e44f552ce3cc6cf3d660214e4a0c685eeb116e380f03f39328b2deb632f3613c48e8ca504ea7cd5cd48799649c'
