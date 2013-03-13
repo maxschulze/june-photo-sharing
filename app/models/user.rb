994b8b17d02095cb0ea9ec02e34b5dfd325ccfe5
Max Schulze <max@mo-stud.io>
2013-03-13 12:03:29 +0100

    installing devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :timeoutable,
+  devise :invitable, :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  has_many :invitations, :class_name => self.to_s, :as => :invited_by
 end
