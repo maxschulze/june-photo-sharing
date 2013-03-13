b52f692e09381832546ed3c8e85786b851cdb874
Max Schulze <max@mo-stud.io>
2013-03-13 12:02:21 +0100

    make the users controller work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
   load_and_authorize_resource
   
   def index
-    
+    @users = User.order('created_at desc').all
   end
   
 end
