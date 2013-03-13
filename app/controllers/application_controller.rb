9c6306db18642ab93f043889452d368b845b356f
Max Schulze <max@mo-stud.io>
2013-03-13 12:03:39 +0100

    make sure only authorized can invite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  check_authorization
   
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+  
+  protected
+  
+  def authenticate_inviter!
+    if can?(:manage, User)
+      true
+    else
+      raise CanCan::AccessDenied.new("You don't have permission to send invites.")
+    end
   end
 end
