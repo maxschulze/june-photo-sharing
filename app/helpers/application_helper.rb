05730f24b4427904ba4b9f381cfdda48518d9f31
Max Schulze <max@mo-stud.io>
2013-03-13 15:49:14 +0100

    display a sign out button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
-  
+
   def nav_link(name, options = {})
+    url = options.delete(:url)
+    controller = options.delete(:controller)
+
     klasses = ''
-    klasses << " active" if request.path == options[:url] or controller_name == options[:controller]
+    klasses << " active" if request.path == url or (controller.present? and controller_name == controller)
     content_tag(:li, class: klasses) do
-      link_to name, options[:url]
+      link_to name, url, options
     end
   end
-  
+
 end
