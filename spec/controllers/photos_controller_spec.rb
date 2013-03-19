0a78871ca4ec9f5621fde17608401bb529767831
Max Schulze <max@mo-stud.io>
2013-03-19 18:02:31 +0100

    adding next / prev arrows

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -14,5 +14,29 @@ describe PhotosController do
     end
 
   end
+  
+  describe "GET /photos/show" do
+    
+    it "should respond with the matching photo" do
+      @photo = create(:photo)
+      get :show, :id => @photo.to_param
+      
+      response.should render_template('photos/show')
+      assigns[:photo].id.should == @photo.id
+    end
+    
+    it "should define the next and prev photo" do
+      3.times { create(:photo) }
+      @photos = Photo.overview.all
+      
+      get :show, :id => @photos.second.to_param
+      
+      response.should be_success
+      
+      assigns[:prev].id.should == @photos.first.id
+      assigns[:next].id.should == @photos.last.id
+    end
+    
+  end
 
 end
