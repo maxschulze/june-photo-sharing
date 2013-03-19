791806065485322066ca7a8f4a019db15d97a3cf
Max Schulze <max@mo-stud.io>
2013-03-19 22:17:05 +0100

    photos now capture when they are taken and the overview is ordered by the date of when a photo was taken

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
+require 'exifr'
+
 class Photo < ActiveRecord::Base
   attr_accessible :description, :title, :user_id, :image
 
@@ -5,7 +7,20 @@ class Photo < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
   
-  scope :overview,  order('created_at desc')
-  scope :previous,  lambda {|current| where('created_at > ?', current.created_at) }
-  scope :next,      lambda {|current| where('created_at < ?', current.created_at) }
+  scope :overview,  order('taken_at desc')
+  scope :previous,  lambda {|current| where('taken_at > ?', current.created_at) }
+  scope :next,      lambda {|current| where('taken_at < ?', current.created_at) }
+  
+  before_save :extract_taken_at
+  
+  private
+  
+  def extract_taken_at
+    exif_file = EXIFR::JPEG.new(image.file.path) rescue nil
+    
+    if exif_file.present? and exif_file.exif?
+      self.taken_at = exif_file.date_time
+    end
+  end
+  
 end
