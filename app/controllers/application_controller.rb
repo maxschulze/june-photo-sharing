370ea8eb1eeaaeeb541f325eebc82b02b022c6ba
Max Schulze <max@mo-stud.io>
2013-03-29 15:16:28 +0100

    redirect all heroku users to new site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :switch_heroku
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
   
   protected
+  
+  def switch_heroku
+    if request.url ~= /heroku/
+      redirect_to "http://server.maxschulze.com#{request.path}"
+      return
+    end
+  end
   
   def authenticate_inviter!
     if can?(:manage, User)
