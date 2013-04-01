735aa80ae065f4b060343eff64cbd16656a8e58a
Max Schulze <max@mo-stud.io>
2013-04-01 21:02:35 +0200

    revert the date / datetime change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401183137) do
+ActiveRecord::Schema.define(:version => 20130401190203) do
 
   create_table "photos", :force => true do |t|
     t.string   "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(:version => 20130401183137) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "image"
-    t.date     "taken_at"
+    t.datetime "taken_at"
   end
 
   create_table "users", :force => true do |t|
